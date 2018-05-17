b1738ced919fa9ab289a116adcf7b5f7a29a2c9a
brianetlab <brianetlab@gmail.com>
2018-05-17 21:08:17 +0700

    Update Database COnnection

--- dbconfig-laptop.php
+++ dbconfig-laptop.php
@@ -1,9 +1,9 @@
 
 <?php
-	$DB_HOST = 'localhost';
-	$DB_USER = 'root';
-	$DB_PASS = '';
-	$DB_NAME = 'leppi';
+	$DB_HOST = 'ipjpi.org';
+	$DB_USER = 'u7843739_leppi';
+	$DB_PASS = 'leppi';
+	$DB_NAME = 'u7843739_leppi';
 	
 	try{
 		$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-	$server='localhost';
-	$user='root';
-	$pass='';
-	$db='leppi';
+	$server='ipjpi.org';
+	$user='u7843739_leppi';
+	$pass='leppi';
+	$db='u7843739_leppi';
 
 	$con=mysql_connect($server,$user,$pass);
 	if($con){
